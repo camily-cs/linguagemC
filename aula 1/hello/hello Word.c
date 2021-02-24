348e6ddd7c658932ebc57a5dccb393987d491e1e
camily-cs <79452420+camily-cs@users.noreply.github.com>
2021-02-24 07:05:13 -0300

    aula 1

---
+++ aula 1/hello/hello Word.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	/*Aula 1-UC9
+	prof José de Assis */
+	
+int main(int argc, char *argv[]) {
+	printf("hello world");
+	system("pause");
+	return 0;
+	
+}
+
