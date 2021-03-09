c77b19d81439d7c69968f87a88d3db8333444d90
Camily Cruz da Silva <79452420+camily-cs@users.noreply.github.com>
2021-03-09 17:23:41 -0300

    Adicionei aula 1

---
+++ aula 1/fundamentos/fundamentos.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+	/* aula 1-UC9 - fundamentos da linguagem C
+ 	@author Camily Cruz */
+
+int main(int argc, char *argv[]) {
+	//A linha abaixo configura o idioma para PT-BR
+	setlocale(LC_ALL, "Portuguese");
+	//Abaixo modificamos a cor do cmd,sendo 1ºfundo e 2ºtexto
+	//cor de fundo:
+	system("color 5F");
+	printf("Hello world\n");
+	printf("Camily Cruz\n");
+	//Na linha abaixo demos uma pausa no cmd
+	system("pause");
+	//Na linha abaixo limpamos a tela do cmd
+	system("cls");
+	//cor do texto:
+	system("color 10");
+	//abaixo mostramos mais opções de comandos de cores
+	printf("Código de cores:\n\n");
+	printf("0 - preto\n");
+	printf("1 - azul\n");
+	printf("2 - verde\n");
+	printf("3 - azul claro\n");
+	printf("4 - vermelho\n");
+	printf("5 - roxo\n");
+	printf("6 - amarelo\n");
+	printf("7 - branco\n");
+	printf("8 - cinza\n");
+	printf("9 - azul claro\n");
+	printf("A - verde claro\n");
+	printf("B - verde água claro\n");
+	printf("C - vermelho claro\n");
+	printf("D - lilás\n");
+	printf("E - amarelo\n");
+	printf("F - branco brilhante\n");
+	system("pause");
+	return 0;
+}
---
+++ aula 1/hello/hello World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	/*Aula 1-UC9
+	@author Camily Cruz */
+	
+int main(int argc, char *argv[]) {
+	printf("hello world");
+	system("pause");
+	return 0;	
+}
+
